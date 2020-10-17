012e0b6f007da264ac8cca5da0c8a17f322f8de5
robouden <rob@yr-design.biz>
2020-10-17 14:55:12 +0900

    Updated version number in serial output on startup

--- src/DWP.ino
+++ src/DWP.ino
@@ -8,6 +8,7 @@
  contact rob@yr-design.biz
  
  revisions:
+ V2.0.8   2020-10-17 intergrated lastest beat detection from Julian and Camilo
  V2.0.7   2020-10-08 added test for startup calculating the startup deley ansd storing in EEPROM
  V2.0.6   2020-17-09 added debouncer for cleaner signal of red switch
  V2.0.5   2020-13-09 pin assigned for other pins
@@ -56,7 +57,7 @@ To be done (2020-10-08):
 
  */
 
-static char VERSION[] = "V2.0.7";;
+static char VERSION[] = "V2.0.8";;
 
 //set drivers 
   #include <SPI.h>
@@ -105,156 +106,157 @@ static char VERSION[] = "V2.0.7";;
         RGBLED rgbLedBeat(2,3,4,COMMON_CATHODE);
         RGBLED rgbLedMotor(5,6,9,COMMON_CATHODE);
 
+        //Audio setup
         AudioInputI2S       audioInput;
-AudioOutputI2S      headphones;
-AudioMixer4         monoMix;
-AudioFilterBiquad   LPfilter;
-AudioAnalyzeFFT1024  myFFT;
-AudioEffectDelay    delayForL;
-AudioEffectDelay    delayForR;
+        AudioOutputI2S      headphones;
+        AudioMixer4         monoMix;
+        AudioFilterBiquad   LPfilter;
+        AudioAnalyzeFFT1024  myFFT;
+        AudioEffectDelay    delayForL;
+        AudioEffectDelay    delayForR;
 
-AudioConnection c0(audioInput, 0, monoMix, 0);
-AudioConnection c1(audioInput, 1, monoMix, 1);
-AudioConnection c2(monoMix, 0, LPfilter, 0);
-AudioConnection c3(LPfilter, 0, myFFT, 0);
-AudioConnection c4(audioInput, 0, delayForL, 0);
-AudioConnection c5(audioInput, 1, delayForR, 1);
-AudioConnection c6(delayForL, 0, headphones, 0);
-AudioConnection c7(delayForR, 0, headphones, 1);
+        AudioConnection c0(audioInput, 0, monoMix, 0);
+        AudioConnection c1(audioInput, 1, monoMix, 1);
+        AudioConnection c2(monoMix, 0, LPfilter, 0);
+        AudioConnection c3(LPfilter, 0, myFFT, 0);
+        AudioConnection c4(audioInput, 0, delayForL, 0);
+        AudioConnection c5(audioInput, 1, delayForR, 1);
+        AudioConnection c6(delayForL, 0, headphones, 0);
+        AudioConnection c7(delayForR, 0, headphones, 1);
 
-// Specify which audio card we're using
-AudioControlSGTL5000 audioShield;
+        // Specify which audio card we're using
+        AudioControlSGTL5000 audioShield;
 
-// Define global constants here
-const int myInput = AUDIO_INPUT_LINEIN; // Are we using mic or line?
-const int numReadings = 7; // How many ODFs are we using for average?
-const int noBins = 512; // number of real bins used in the FFT (total number of bins should be double this)
-const int LPorder = 7; // Linear predictor order
-const int led = 13;
-const int ONtime_mills = 10;   // time for better delay function
-unsigned long ledStarted = 0;
+        // Define global constants here
+        const int myInput = AUDIO_INPUT_LINEIN; // Are we using mic or line?
+        const int numReadings = 7; // How many ODFs are we using for average?
+        const int noBins = 512; // number of real bins used in the FFT (total number of bins should be double this)
+        const int LPorder = 7; // Linear predictor order
+        const int led = 13;
+        const int ONtime_mills = 10;   // time for better delay function
+        unsigned long ledStarted = 0;
 
-// Specifty global variables here
-unsigned long curDel = 0;     // Current delay
-double avgDelay = 0.0;  // Average delay
-float prevMags[noBins][LPorder]; // Previous magnitudes for LP prediction
-double ks[LPorder]; // weigths of the previous samples in the prediction of the current
-float minLevel = 10; //signal must be higher that 1% to report a beat
-
-
-
-unsigned int minDelay = 29;   // How long should we wait at least before the next beat?
-double lambda = .6; // weight of the median
-double alpha = .6;  // weight of the mean
-double m = 0.0;     // the mean of the previous ODFs
-double median = 0.0;     // the median of the previous ODFs
-
-void lp(int k) {
-  // Computes the linear prediction weights for the previous values of a given bin k
-  float wk1[LPorder];
-  float wk2[LPorder];
-  float wkm[LPorder];
-
-  wk1[0] = prevMags[k][0];
-  wk2[LPorder - 2] = prevMags[k][LPorder - 1];
-
-  for (int i = 1; i < LPorder - 1; ++i) {
-    wk1[i] = prevMags[k][i];
-    wk2[i - 1] = prevMags[k][i];
-  }
-
-  for (int i = 0; i < LPorder; ++i) {
-    double num = 0.0;
-    double denom = 0.0000000000000001; // to avoid NaNs
-
-    for (int j = 0; j < (LPorder - i); ++j) {
-      num += wk1[j] * wk2[j];
-      denom += wk1[j] * wk1[j] + wk2[j] * wk2[j];
-    }
-    ks[i] = 2.0 * num / denom;
-
-    for (int j = 0; j < i; ++j) {
-      ks[j] = wkm[j] - ks[i] * wkm[i - j];
-    }
-    for (int j = 0; j <= i; ++j) {
-      wkm[j] = ks[j];
-    }
-    for (int j = 0; j < (LPorder - i - 1); ++j) {
-      wk1[j] -= wkm[i] * wk2[j];
-      wk2[j] = wk2[j + 1] - wkm[i] * wk1[j + 1];
-    }
-  }
-}
-
-        //display magnet info  setup 
-        byte previousState = HIGH;         // what state was the button last time
-        unsigned int count = 0;            // how many times has it changed to low
-        unsigned long countAt = 0;         // when count changed
-        unsigned int countPrinted = 0;     // last count printed
-   
-void setup() {
-  //serial start 
-    Serial.begin(9600);
-    delay(100);
-  
-  //EEPROM setup for Teensy4
-    EEPROM.setMemPool(memBase, EEPROMSizeTeensy40);
-    EEPROM.setMaxAllowedWrites(maxAllowedWrites);    
-    addressLong   = EEPROM.getAddress(sizeof(long));
-    trigger_level = EEPROM.readLong(addressLong);
+        // Specifty global variables here
+        unsigned long curDel = 0;     // Current delay
+        double avgDelay = 0.0;  // Average delay
+        float prevMags[noBins][LPorder]; // Previous magnitudes for LP prediction
+        double ks[LPorder]; // weigths of the previous samples in the prediction of the current
+        float minLevel = 10; //signal must be higher that 1% to report a beat
 
 
-  // setup pins for switches
-    // // beatleds
-    //   pinMode(beatRedPin, OUTPUT);
-    //   pinMode(beatGreenPin, OUTPUT);
-    //   pinMode(beatBluePin, OUTPUT);
-    // // motorleds
-    //   pinMode(motorRedPin, OUTPUT);
-    //   pinMode(motorGreenPin, OUTPUT);
-    //   pinMode(motorBluePin, OUTPUT);
-    // misc pins
-      pinMode(outputPin, OUTPUT);
-      pinMode(magnetPin, INPUT_PULLUP);
-      pinMode(potPin, INPUT);
 
-    delay(1000);
-    Serial.println("Magnet switch setup done");
-    //display status on serial
-      Serial.print("Version=");
-      Serial.println(VERSION);
-      Serial.print("Setup Mode=");
-      Serial.println(setupmode);
-      Serial.print("Trigger_level=");
-      Serial.println((trigger_level)*10000);
-      Serial.print("magnetOn=");
-      Serial.println(magnetOn);
+        unsigned int minDelay = 29;   // How long should we wait at least before the next beat?
+        double lambda = .6; // weight of the median
+        double alpha = .6;  // weight of the mean
+        double m = 0.0;     // the mean of the previous ODFs
+        double median = 0.0;     // the median of the previous ODFs
 
-  
+        void lp(int k) {
+          // Computes the linear prediction weights for the previous values of a given bin k
+          float wk1[LPorder];
+          float wk2[LPorder];
+          float wkm[LPorder];
 
-  // Audio connections require memory to work.
-  AudioMemory(150); // To Do: check the actual memory used and free the rest
+          wk1[0] = prevMags[k][0];
+          wk2[LPorder - 2] = prevMags[k][LPorder - 1];
 
-  // Enable the audio shield and set the output volume.
-  audioShield.enable();
-  audioShield.audioPreProcessorEnable();
-  audioShield.autoVolumeControl(2, 3, 0, -30, 5, 50); //enable compressor
+          for (int i = 1; i < LPorder - 1; ++i) {
+            wk1[i] = prevMags[k][i];
+            wk2[i - 1] = prevMags[k][i];
+          }
 
-  audioShield.inputSelect(myInput);
-  audioShield.volume(.3);  //0.8 is the max undistorted output on the headphones
+          for (int i = 0; i < LPorder; ++i) {
+            double num = 0.0;
+            double denom = 0.0000000000000001; // to avoid NaNs
 
-  // reduce the gain on mixer channels
-  monoMix.gain(0, 0.5);
-  monoMix.gain(1, 0.5);
+            for (int j = 0; j < (LPorder - i); ++j) {
+              num += wk1[j] * wk2[j];
+              denom += wk1[j] * wk1[j] + wk2[j] * wk2[j];
+            }
+            ks[i] = 2.0 * num / denom;
 
-  // Butterworth filter, 12 db/octave
-  LPfilter.setLowpass(0, 4000, .6);
+            for (int j = 0; j < i; ++j) {
+              ks[j] = wkm[j] - ks[i] * wkm[i - j];
+            }
+            for (int j = 0; j <= i; ++j) {
+              wkm[j] = ks[j];
+            }
+            for (int j = 0; j < (LPorder - i - 1); ++j) {
+              wk1[j] -= wkm[i] * wk2[j];
+              wk2[j] = wk2[j + 1] - wkm[i] * wk1[j + 1];
+            }
+          }
+        }
 
-  // Configure the window algorithm to use
-  myFFT.windowFunction(AudioWindowHanning1024);
+                //display magnet info  setup 
+                byte previousState = HIGH;         // what state was the button last time
+                unsigned int count = 0;            // how many times has it changed to low
+                unsigned long countAt = 0;         // when count changed
+                unsigned int countPrinted = 0;     // last count printed
+          
+        void setup() {
+          //serial start 
+            Serial.begin(9600);
+            delay(100);
+          
+          //EEPROM setup for Teensy4
+            EEPROM.setMemPool(memBase, EEPROMSizeTeensy40);
+            EEPROM.setMaxAllowedWrites(maxAllowedWrites);    
+            addressLong   = EEPROM.getAddress(sizeof(long));
+            trigger_level = EEPROM.readLong(addressLong);
 
-  delayForL.delay(0, 1000 * noBins / 44100);
-  delayForR.delay(0, 1000 * noBins / 44100);
+
+          // setup pins for switches
+            // // beatleds
+            //   pinMode(beatRedPin, OUTPUT);
+            //   pinMode(beatGreenPin, OUTPUT);
+            //   pinMode(beatBluePin, OUTPUT);
+            // // motorleds
+            //   pinMode(motorRedPin, OUTPUT);
+            //   pinMode(motorGreenPin, OUTPUT);
+            //   pinMode(motorBluePin, OUTPUT);
+            // misc pins
+              pinMode(outputPin, OUTPUT);
+              pinMode(magnetPin, INPUT_PULLUP);
+              pinMode(potPin, INPUT);
+
+            delay(1000);
+            Serial.println("Magnet switch setup done");
+            //display status on serial
+              Serial.print("Version=");
+              Serial.println(VERSION);
+              Serial.print("Setup Mode=");
+              Serial.println(setupmode);
+              Serial.print("Trigger_level=");
+              Serial.println((trigger_level)*10000);
+              Serial.print("magnetOn=");
+              Serial.println(magnetOn);
+
+          
+
+          // Audio connections require memory to work.
+          AudioMemory(150); // To Do: check the actual memory used and free the rest
+
+          // Enable the audio shield and set the output volume.
+          audioShield.enable();
+          audioShield.audioPreProcessorEnable();
+          audioShield.autoVolumeControl(2, 3, 0, -30, 5, 50); //enable compressor
+
+          audioShield.inputSelect(myInput);
+          audioShield.volume(.3);  //0.8 is the max undistorted output on the headphones
+
+          // reduce the gain on mixer channels
+          monoMix.gain(0, 0.5);
+          monoMix.gain(1, 0.5);
+
+          // Butterworth filter, 12 db/octave
+          LPfilter.setLowpass(0, 4000, .6);
+
+          // Configure the window algorithm to use
+          myFFT.windowFunction(AudioWindowHanning1024);
+
+          delayForL.delay(0, 1000 * noBins / 44100);
+          delayForR.delay(0, 1000 * noBins / 44100);
     
 
     //test for collecting time for delay start_motor and reed_read (new name for reed activity.)
