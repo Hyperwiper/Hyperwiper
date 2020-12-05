247d9d1e02663af81b5d245216e68f9c3c36298c
robouden <rob@yr-design.biz>
2020-12-05 21:29:06 +0900

    Updated board with Xbee's

--- src/DWP.ino
+++ src/DWP.ino
@@ -360,10 +360,12 @@ void loop() {
         pot_old_read=potRead;
         Serial.print("new potential meter level =");
         Serial.println(potRead);
+        Serial.print("\t");
   //convert run_reed_leave_time to half of one_wipe_time
         int one_wipe_time_procentage= (run_reed_leave_time/2)*potRead/100;
         Serial.print("one wipe time divided by potread =");
         Serial.println(one_wipe_time_procentage);
+        Serial.print("\t");
       }
    
   //subtract from calculated expected beat
