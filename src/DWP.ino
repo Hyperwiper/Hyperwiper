4e1fc9655071eca2fc0870540652e6408baedb24
robouden <rob@yr-design.biz>
2022-07-19 08:01:27 +0900

    Updated Version number

--- src/DWP.ino
+++ src/DWP.ino
@@ -8,7 +8,8 @@
  contact rob@yr-design.biz
  
  revisions:
- V2.6.2   2022-07-13 Reset time=po after delay is done same for time_interbeat
+ V2.6.3   2022-07-19 Updated version number to reflect newest code
+ V2.6.2   2022-07-13 Reset time=0 after delay is done same for time_interbeat
  V2.6.1   2022-07-12 Updated many file and started using VSCode instead of VSCodium.  
  V2.6.0   2022-06-04 Cleanup
  V2.5.9   2022-06-01 Setup test for interbeats
@@ -112,7 +113,7 @@ To be done (2021-03-20):
 
  */
 
-static char VERSION[] = "V2.6.2";
+static char VERSION[] = "V2.6.3";
 
 //set drivers 
   #include <SPI.h>
